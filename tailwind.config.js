38cc5c847fa34cbec61eeca620e43aecb893fd83
malvaa <malvarez@aluxion.com>
2024-01-20 21:10:08 -0300

    Add: theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: '#A729F5',
+        white: '#FFFFFF',
+        black: '#000000',
+        navy: {
+          dark: '#313E51',
+          navy: '#3B4D66',
+          grey: '#626C7F'
+        },
+        grey: '#F4F6FA',
+        bluish: '#ABC1E1',
+        green: '#26D782',
+        red: '#EE5454',
+      }
+    },
   },
   darkMode: 'class',
   plugins: [],
