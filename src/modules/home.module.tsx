3f27d98c5fbec7bc2868f71cf4c917c3b7a0d519
malvaa <malvarez@aluxion.com>
2024-01-29 00:27:02 -0300

    Add: link to quiz

--- src/modules/home.module.tsx
+++ src/modules/home.module.tsx
@@ -3,11 +3,14 @@ import "./home.style.css";
 import { IQuizCategoryFormatted } from "../interfaces/quiz-data.interface";
 import { OptionItemComponent } from "../components/option-item/option-item.component";
 import { SVGIcon } from "../UI/icon-component/icon.component";
+import { useAppRouter } from "../hooks/useAppRouter";
+import { useNavigate } from "react-router-dom";
 import { useQuizData } from "../hooks/useQuizData";
 
 export const HomeModule = () => {
   const { quizList } = useQuizData();
-
+  const { publicRoutes } = useAppRouter();
+  const navigate = useNavigate();
   return (
     <div className="home-screen-container">
       <section className="home-text">
@@ -25,7 +28,9 @@ export const HomeModule = () => {
         {quizList &&
           quizList.map((item: IQuizCategoryFormatted) => (
             <OptionItemComponent
-              onClick={() => {}}
+              onClick={() => {
+                navigate(`${publicRoutes.QUIZZ_LINK}${item.category}`);
+              }}
               value={item.title}
               leftIcon={<SVGIcon icon={item.icon} />}
               leftBg={item.backgroundColor}
