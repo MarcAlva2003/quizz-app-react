cfa068f831f94e4b959cdc042cf9533d94e6a2c2
malvaa <malvarez@aluxion.com>
2024-01-29 00:25:59 -0300

    Add: quiz route

--- src/hooks/useAppRouter.tsx
+++ src/hooks/useAppRouter.tsx
@@ -6,6 +6,7 @@ import { QuizzPage } from "../pages/quizz.page";
 export interface IPublicRoutes {
   HOME: string;
   QUIZZ: string;
+  QUIZZ_LINK: string;
   NOT_FOUND: string;
 }
 
@@ -13,6 +14,7 @@ export const useAppRouter = () => {
   const publicRoutes: IPublicRoutes = {
     HOME: "/",
     QUIZZ: "/quizz/:category",
+    QUIZZ_LINK: "/quizz/",
     NOT_FOUND: "*",
   };
 
