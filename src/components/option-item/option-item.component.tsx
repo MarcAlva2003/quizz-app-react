0859cd1fd35cf049174d59bb7f3de26d3899f343
malvaa <malvarez@aluxion.com>
2024-01-29 00:25:50 -0300

    Add: optoin item component quiz config

--- src/components/option-item/option-item.component.tsx
+++ src/components/option-item/option-item.component.tsx
@@ -1,5 +1,9 @@
 import "./option-item.style.css";
 
+import { SVGIcon } from "../../UI/icon-component/icon.component";
+import correctIcon from "../../assets/images/icon-correct.svg";
+import errorIcon from "../../assets/images/icon-error.svg";
+
 interface IOptionItemComponent {
   onClick: () => void;
   value: string;
@@ -7,22 +11,45 @@ interface IOptionItemComponent {
   optionLetter?: string;
   focus?: boolean;
   leftBg?: string;
+  correct?: boolean;
+  showResults?: boolean;
 }
 
 export const OptionItemComponent = (props: IOptionItemComponent) => {
-  const { onClick, value, focus, leftIcon, optionLetter, leftBg } = props;
+  const {
+    onClick,
+    value,
+    focus,
+    leftIcon,
+    optionLetter,
+    leftBg,
+    correct,
+    showResults,
+  } = props;
   return (
-    <button className={`option-item-component ${focus ? 'option-item-component--focus' : ''}`} onClick={onClick}>
-      <div
-        className="option-item--left bg-grey"
-        style={{
-          backgroundColor: leftBg,
-        }}
-      >
-        {leftIcon ? leftIcon : <p>{optionLetter}</p>}
+    <button
+      className={`option-item-component ${
+        focus ? "option-item-component--focus" : ""
+      } ${correct && showResults ? "option-item-component--correct" : ""} ${
+        !correct && showResults ? "option-item-component--incorrect" : ""
+      } `}
+      onClick={onClick}
+    >
+      <div className="option-item--left">
+        <div
+          className="option-item--left__icon bg-grey"
+          style={{
+            backgroundColor: leftBg,
+          }}
+        >
+          {leftIcon ? leftIcon : <p>{optionLetter}</p>}
+        </div>
+        <div className="option-item--left__title">
+          <p>{value}</p>
+        </div>
       </div>
-      <div className="option-item--title">
-        <p>{value}</p>
+      <div className="answer-icon">
+        {showResults && <SVGIcon icon={correct ? correctIcon : errorIcon} />}
       </div>
     </button>
   );
