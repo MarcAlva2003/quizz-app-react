430eb97452d4331076cd88cce87238c69ebf5934
malvaa <malvarez@aluxion.com>
2024-01-29 01:45:28 -0300

    Fix: move in progress modal to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import "./app.css";
 
 import { HeaderComponent } from "./components/header/header.component";
-import { InProgressModal } from "./components/in-progress-modal/in-progress-modal.component";
 import { QuizContextProvider } from "./context/quiz.context";
 import { useAppRouter } from "./hooks/useAppRouter";
-import { useState } from "react";
 
 function App() {
   const { publicRouter } = useAppRouter();
-  const [showProgressModal, setProgressModal] = useState<boolean>(false);
   return (
     <div className="bg-grey w-full min-h-screen dark:bg-navy-dark app-content-container">
       <QuizContextProvider>
@@ -16,12 +13,6 @@ function App() {
         <section className="w-full app-routes-container">
           {publicRouter}
         </section>
-        <InProgressModal
-          isOpen={showProgressModal}
-          onClose={() => {
-            setProgressModal(false);
-          }}
-        />
       </QuizContextProvider>
     </div>
   );
